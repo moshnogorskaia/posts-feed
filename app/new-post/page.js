c8107a8306e68698f0691f3f7c56252d20a4f192
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-13 20:33:16 +0100

    Add useActionState to the form

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,14 +1,36 @@
+'use client';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 import FormSubmit from '@/components/form-submit';
+import { useActionState } from 'react';
 
 export default function NewPostPage() {
+  const [state, formAction] = useActionState(createPost, {});
+
   async function createPost(formData) {
     'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
-    
+
+    const errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push('Title is required!');
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push('Content is required!');
+    }
+
+    if (!image) {
+      errors.push('Image is required!');
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
     await storePost({ title, imageUrl: '', content, userId: 1 });
 
     redirect('/feed');
@@ -18,7 +40,7 @@ export default function NewPostPage() {
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
